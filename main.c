4bdf74ca78eb745d28e23af27856a798f9c41c49
Gaurav Duggal <gaurav.duggal@tonboimaging.com>
2016-05-04 19:38:36 +0530

    added gps_ring struct and working prototype

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #define RING_LENGTH 4096
+#define DEBUG
+#ifdef DEBUG
+    #define DBG(fmt, args...);   printf("" fmt, ##args)
+#else   
+    #define DBG(fmt, args...)
+#endif
 
 
 
@@ -11,6 +17,12 @@ typedef struct n
 	uint8_t data;
 	struct n *next;
 }node;
+
+typedef struct buf
+{
+	node* w_ptr;
+	node* r_ptr;
+}gps_ring;
 
 node* init_ring(uint32_t num)
 {
@@ -23,20 +35,33 @@ for (iter=start;i<num;i++)
 	iter->next = (node*) malloc(sizeof(node));
 	iter->data = i;
 	iter=iter->next;
+	DBG("%d\n",i);
 }
 iter->next=start;
 return start;
 
 }
 
-uint32_t ring_len(node* start)
+void gps_init(gps_ring* gps)
+{
+	gps->w_ptr=NULL;
+	gps->r_ptr=NULL;
+	gps->w_ptr=init_ring(RING_LENGTH);
+	gps->r_ptr=gps->w_ptr;
+
+}
+
+uint32_t ring_len(gps_ring *gps)
 {
 	node* iter=NULL;
 	uint32_t len=0;
-	for (iter=start->next; (iter!=start); iter=iter->next)
+	iter=gps->w_ptr->next;
+	#if 1
+	for (; (iter!=gps->w_ptr); iter=iter->next)
 	{
 		len++;
 	}
+	#endif
 return len;
 }
 
@@ -47,53 +72,50 @@ uint8_t read()
 	//return rand(); 
 }
 
-node* fill_ring(uint8_t data,node* write)
+void fill_ring(uint8_t data,gps_ring *gps)
 {
-	write->data = data;
-	return write->next;
+	gps->w_ptr->data = data;
+	gps->w_ptr=gps->w_ptr->next;
 }
 
-node* read_ring(uint8_t* data,node* read)
+read_ring(uint8_t* data,gps_ring *gps)
 {
-	*data = read->data;
-	return read->next;
+	*data = gps->r_ptr->data;
+	gps->r_ptr=gps->r_ptr->next;
 }
 
 
 int main()
 {
-node* r_ptr=NULL;
-node* w_ptr=NULL;
-uint8_t data=0;
+uint8_t data;
+uint32_t i=0;
 srand(time(NULL));
+gps_ring gps1;
+gps_init(&gps1);
+printf("len %d\n",ring_len(&gps1));
 
-node* start = init_ring(RING_LENGTH);
-r_ptr=start;
-w_ptr=start;
-
-printf("len %d\n",ring_len(start));
-int i=0;
-
+#if 1
 while(1)
 {
 	i=((uint8_t)rand())/4;
 	printf("bytes to be read %d\n",i);
 	while(i--)
-		w_ptr = fill_ring(read(),w_ptr);
+		fill_ring(read(),&gps1);
 	
-	while(r_ptr!=w_ptr)
+	while(gps1.r_ptr!=gps1.w_ptr)
 	{
-		r_ptr = read_ring(&data , r_ptr);
+	    read_ring(&data , &gps1);
 		printf("%d\n",data);
 		usleep(10000);
 	}
 
 }
+#endif
 return 0;
 }
 
 #if 0
-void find_gps_str(node*node* read, uint8_t* str)
+void find_gps_str(node* read, uint8_t* str)
 {
 	node* iter = read;
 	while(iter->data = 0xe0)
